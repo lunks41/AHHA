08069802ab1e7f9355c286b5f9cef3a2fc203977
Harshad Lunkad <56657753+lunks41@users.noreply.github.com>
2024-08-26 15:28:25 +0400

    AHHA_240826.02

---
+++ .svn/pristine/47/473f33d684f0c0485c151401f36e8b6dd045e4e0.svn-base
@@ -0,0 +1,9 @@
+﻿namespace AHHA.Core.Models.Masters
+{
+    public class BankLookupModel
+    {
+        public Int32 BankId { get; set; }
+        public string BankCode { get; set; }
+        public string BankName { get; set; }
+    }
+}
---
+++ AHHA.Domain/Models/Lookup/CurrencyLookupModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AHHA.Core.Models.Masters
+{
+    public class CurrencyLookupModel
+    {
+        public int CurrencyId { get; set; }
+        public string CurrencyCode { get; set; }
+        public string CurrencyName { get; set; }
+    }
+}
