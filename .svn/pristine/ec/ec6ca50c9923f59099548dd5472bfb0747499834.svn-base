0c4e44fa7a697bb1715d0d5d2efe8e5258d66b7f
Harshad Lunkad <56657753+lunks41@users.noreply.github.com>
2024-09-21 20:21:25 +0400

    AHHA_240921.01

---
+++ .svn/pristine/ec/ec6ca50c9923f59099548dd5472bfb0747499834.svn-base
@@ -0,0 +1,9 @@
+﻿namespace AHHA.Core.Models.Masters
+{
+    public class ModuleLookupModel
+    {
+        public Int16 ModuleId { get; set; }
+        public string ModuleCode { get; set; }
+        public string ModuleName { get; set; }
+    }
+}
---
+++ AHHA.Domain/Models/Lookup/TransactionLookupModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AHHA.Core.Models.Masters
+{
+    public class TransactionLookupModel
+    {
+        public Int16 TransactionId { get; set; }
+        public string TransactionCode { get; set; }
+        public string TransactionName { get; set; }
+    }
+}
